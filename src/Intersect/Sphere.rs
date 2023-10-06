692ecdfda56f97d1ba01d1b4de2abebeaf37f292
brosnanyuen <brosnany@uvic.ca>
2023-10-05 21:35:46 -0700

    save

--- src/Intersect/Sphere.rs
+++ src/Intersect/Sphere.rs
@@ -144,9 +144,10 @@ pub fn line_sphere_intersect_batch<Z: arrayfire::RealFloating  >(
 
 	let input_circle_radius  = arrayfire::rows(circle_radius, startseq  as i64,endseq as i64);
 
-    let seqs2 = &[arrayfire::Seq::new(startseq as f64, endseq as f64, 1.0 as f64), arrayfire::Seq::default()];
-    let input_circle_center  = arrayfire::index(circle_center, seqs2);
+    //let seqs2 = &[arrayfire::Seq::new(startseq as f64, endseq as f64, 1.0 as f64), arrayfire::Seq::default()];
+    //let input_circle_center  = arrayfire::index(circle_center, seqs2);
 
+	let input_circle_center  = arrayfire::rows(circle_center, startseq  as i64,endseq as i64);
 
 	line_sphere_intersect(
 		start_line,
