0e6dce08d3535e795e0054f094165cc17d847ade
brosnanyuen <brosnany@uvic.ca>
2023-10-06 13:34:27 -0700

    save

--- src/Intersect/Sphere.rs
+++ src/Intersect/Sphere.rs
@@ -9,13 +9,13 @@ use std::collections::HashMap;
 
 
 
-const TWO_f64: f64 = 2.0;
-const ONE_f64: f64 = 1.0;
-const ZERO_f64: f64 = 0.0;
+const TWO_F64: f64 = 2.0;
+const ONE_F64: f64 = 1.0;
+const ZERO_F64: f64 = 0.0;
 
-const EPSILON_f64: f64 = 1.0e-3;
+const EPSILON_F64: f64 = 1.0e-3;
 
-const ONEMINUSEPSILON_f64: f64 = ONE_f64 - EPSILON_f64;
+const ONEMINUSEPSILON_F64: f64 = ONE_F64 - EPSILON_F64;
 
 const COUNT_LIMIT: u64 = 10000000000;
 
@@ -38,13 +38,13 @@ pub fn line_sphere_intersect<Z: arrayfire::RealFloating<AggregateOutType = Z>  >
 
 
 	let t_dims = arrayfire::Dim4::new(&[1,1,1,1]);
-	let temp_constant = vec![TWO_f64 ];
+	let temp_constant = vec![TWO_F64 ];
 	let mut TWO = arrayfire::Array::new(&temp_constant, t_dims).cast::<Z>();
 
-	let temp_constant = vec![ONE_f64 ];
+	let temp_constant = vec![ONE_F64 ];
 	let mut ONE = arrayfire::Array::new(&temp_constant, t_dims).cast::<Z>();
 
-	let temp_constant = vec![ZERO_f64 ];
+	let temp_constant = vec![ZERO_F64 ];
 	let mut ZERO = arrayfire::Array::new(&temp_constant, t_dims).cast::<Z>();
 
 
